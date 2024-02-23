2af2dd9f1cd72ee1bb057f16ca8dff43736efda3
zlyms <110710393+zlyms@users.noreply.github.com>
2024-02-24 00:34:51 +0800

    Add files via upload

---
+++ DLinkList/DLinkList.c
@@ -0,0 +1,133 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef int Elemtype;
+
+// 定义双链表结点结构体
+typedef struct DLNode {
+	struct DLNode* prior; // 指向前一个结点的指针
+	Elemtype data; // 数据域
+	struct DLNode* next; // 指向后一个结点的指针
+} DLNode, * DLinkList;
+
+// 初始化双链表
+int IniDLinkList(DLinkList* L) {
+	DLNode* p = *L; // 头指针
+	if (p == NULL) {
+		return 0;
+	}
+	p = (DLNode*)malloc(sizeof(DLNode)); // 分配结点内存空间
+	if (p == NULL) {
+		perror("IniLinkLIst:");
+		printf("空间申请失败");
+		return 0;
+	}
+	p->next = NULL;
+	p->prior = NULL;
+	*L = p; // 更新头指针
+	return 1;
+}
+
+// 移动指针到第 i 个结点的位置
+DLNode* MovePoint(DLinkList L, int i) {
+	if (i < 1) {
+		printf("位置错误\n");
+		return NULL;
+	}
+	int j = 0;
+	DLNode* p = L; // 头指针
+	while (p != NULL && j < i - 1) {
+		p = p->next; // 移动指针
+		j++;
+	}
+	if (p == NULL) {
+		printf("位置错误\n");
+		return NULL;
+	}
+	return p;
+}
+
+// 在第 i 个位置插入元素 e
+int DLinkListInser(DLinkList L, int i, Elemtype e) {
+	DLNode* p = MovePoint(L, i); // 找到第 i 个结点的位置
+	if (p == NULL) {
+		return 0;
+	}
+	DLNode* s = (DLNode*)malloc(sizeof(DLNode)); // 创建新结点
+	if (s == NULL) {
+		perror("DLinkListInser:");
+		printf("空间申请失败\n");
+		return 0;
+	}
+	s->data = e; // 设置新结点的数据
+	if (p->next != NULL) {
+		(p->next)->prior = s; // 更新后一个结点的前驱指针
+	}
+	s->next = p->next; // 新结点指向原来第 i 个位置的结点
+	p->next = s; // 前一个结点指向新结点
+	s->prior = p; // 新结点的前驱指向前一个结点
+	return 0;
+}
+
+// 删除第 i 个结点
+int DeleteDLNode(DLinkList L, int i) {
+	DLNode* p = MovePoint(L, i); // 找到第 i 个结点的位置
+	if (p == NULL) {
+		return 0;
+	}
+	DLNode* q = p->next; // 待删除的结点
+	if (q == NULL) {
+		return 0;
+	}
+	if (q->next == NULL) { // 如果待删除的结点是最后一个结点
+		free(q);
+		p->next = NULL;
+		return 1;
+	}
+	q->next->prior = p; // 更新后一个结点的前驱指针
+	p->next = q->next; // 前一个结点指向后一个结点
+	free(q); // 释放待删除结点的内存空间
+	q = NULL;
+	return 1;
+}
+
+// 获取第 i 个结点
+DLNode* GetElem(DLinkList L, int i) {
+	if (i < 1) {
+		printf("查找位置错误\n");
+		return NULL;
+	}
+	DLNode* p = MovePoint(L, i); // 找到第 i-1 个结点的位置
+	if (p == NULL) {
+		printf("查找位置错误\n");
+		return NULL;
+	}
+	DLNode* q = p->next; // 获取第 i 个结点
+	if (q == NULL) {
+		printf("查找位置错误\n");
+		return NULL;
+	}
+	return q;
+}
+
+int main() {
+	DLinkList L;
+	IniDLinkList(&L); // 初始化双链表
+	DLinkListInser(L, 1, 10); // 在第一个位置插入元素 10
+	DLinkListInser(L, 2, 20); // 在第二个位置插入元素 20
+	DLinkListInser(L, 3, 30); // 在第三个位置插入元素 30
+	DLinkListInser(L, 4, 40); // 在第四个位置插入元素 40
+	DeleteDLNode(L, 4); // 删除第四个位置的结点
+	DLinkList p = L->next; // 头节点的下一个结点
+	while (p != NULL) { // 顺序遍历
+		printf("%d\n", p->data);
+		p = p->next;
+	}
+	printf("-----------------------------------------\n");
+	p = GetElem(L, 3); // 获取第三个位置的结点
+	while (p != NULL && p != L) { // 逆序遍历
+		printf("%d\n", p->data);
+		p = p->prior;
+	}
+	return 0;
+}
---
+++ LinkList/LinkList.c
@@ -0,0 +1,206 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int ElemType;
+
+// 定义链表节点结构体
+typedef struct LNode {
+    ElemType data;          // 数据域
+    struct LNode* next;     // 指针域，指向下一个节点
+} LNode, * LinkList;        // 定义节点类型和节点指针类型
+
+// 初始化链表，将头指针置为空
+void InitLinkList(LinkList* head) {
+    *head = NULL;
+}
+//移动指针至i-1个节点
+LNode* MovePoint(LinkList* L, int i)
+{
+    if (i < 1) 
+    {
+        printf("位置错误");
+        return NULL;
+    }
+    int j = 0;
+    LNode* p = *L;
+    while (p != NULL && j < i - 1) 
+    {
+        p = p->next;
+        j++; 
+    }
+    if (p == NULL) 
+    {
+        printf("前驱节点不存在\n");
+        return NULL;
+    }
+    return p;
+}
+//在指定位置i的前面插入节点
+int FrontInsert(LinkList*L,int i, ElemType e)
+{
+    LNode* p = MovePoint(L, i);
+    if (p == NULL) 
+    {
+        return 0;
+    }
+    LNode* s = (LNode*)malloc(sizeof(LNode));
+    if (s == NULL) 
+    {
+        perror("FrontInsert:");
+        printf("申请内存失败\n");
+        return 0;
+    }
+    ElemType temp = p->data;
+    p->data = e;
+    s->data = temp;
+    s->next = p->next;
+    p->next = s;
+    printf("前插成功\n");
+    return 1;
+}
+// 在链表的第 i 个位置插入元素 e
+int InsertList(LinkList* L, int i, ElemType e) {
+    if (i < 1) {
+        return 0; // 插入位置非法
+    }
+    LinkList p = *L; // p 指向头节点
+    int j = 0;
+    while (p != NULL && j < i - 1) {
+        p = p->next; // 找到第 i-1 个节点
+        j++;
+    }
+    if (p == NULL) {
+        return 0; // 插入位置不存在
+    }
+    // 创建新节点
+    LNode* s = (LNode*)malloc(sizeof(LNode));
+    if (s == NULL) {
+        return 0; // 内存分配失败
+    }
+    s->data = e;
+    s->next = p->next; // 将新节点的 next 指针指向第 i 个节点
+    p->next = s; // 将第 i-1 个节点的 next 指针指向新节点
+    return 1; // 插入成功
+}
+//删除第i个位置的前一个节点
+int FrontDelete(LinkList* L, int i, ElemType* e)
+{
+    LNode* p = MovePoint(L, i);
+    if (p == NULL)
+    {
+        return 0;
+    }
+    LNode* s = p->next;
+    ElemType temp = p->data;
+    p->data = (p->next)->data;
+    p->next = s->next;
+    * e = temp;
+    free(s);
+    s = NULL;
+
+    return *e;//返回删除元素
+}
+//后删，删除第i-1个位置的后一个节点也就是i
+int DeleteList(LinkList*L,int i, ElemType* e)
+{
+    if (i < 1) {
+        return 0; // 插入位置非法
+    }
+    LinkList p = *L; // p 指向头节点
+    int j = 0;
+    while (p != NULL && j < i - 1) {
+        p = p->next; // 找到第 i-1 个节点
+        j++;
+    }
+    if (p == NULL) //指定位置前驱节点不存在
+    {
+        printf("指定节点不存在\n");
+        return 0;
+    }
+    if (p->next == NULL)//指定位置无节点
+    {
+        printf("指定节点不存在\n");
+        return 0;
+    }
+    LNode* s = p->next;
+    *e = (p->next)->data;
+    p->next = (p->next)->next;
+    free(s);
+    printf("删除成功\n");
+    return *e;//返回删除元素
+}
+int getValue(LinkList*L, ElemType e)
+{
+    LNode* p = (*L)->next;
+    int i = 1;
+    int flase = 0;
+    while (p != NULL)
+    {
+        if (p->data != e) 
+        {
+            p = p->next;
+            
+        }
+        else
+        {
+            printf("在%d个元素的值是：%d\n", i, e);
+            flase++;
+            p = p->next;
+        }
+        i++;
+    }
+    if (!flase) 
+    {
+        printf("没有找到");
+        return 0;
+    }
+    return 1;
+}
+
+// 获取链表中第 i 个位置的元素值
+int GetElem(LinkList L, int i, ElemType* e) {
+    if (i < 1) {
+        return 0; // 位置非法
+    }
+    LinkList p = L;
+    int j = 0;
+    while (p != NULL && j < i) {
+        p = p->next; // 遍历链表找到第 i 个节点
+        j++;
+    }
+    if (p == NULL) {
+        return 0; // 第 i 个元素不存在
+    }
+    *e = p->data; // 获取第 i 个元素的值
+    return 1; // 获取成功
+}
+
+int main() {
+  
+    LinkList L;             // 声明链表头指针
+    InitLinkList(&L);       // 初始化链表
+    LNode head = { 3,NULL };
+    L = &head;
+    FrontInsert(&L, 1, 10); // 在第一个位置插入元素 10
+    FrontInsert(&L, 1, 20);
+   
+
+    // 获取链表中第一个元素的值并输出
+    ElemType elem;
+   // FrontDelete(&L, 2, &elem);
+ /*   if (GetElem(L, 1, &elem)) {
+        printf("第一个元素的值是：%d\n", elem);
+    }
+    else {
+        printf("获取元素失败\n");
+    }*/
+    int i = 1;
+    while (GetElem(L,  i, &elem)) //遍历链表中所有元素
+    {
+        printf("第%d个元素的值是：%d\n",i, elem);
+        i++;
+    }
+    getValue(&L, 10);
+
+    return 0;
+}
